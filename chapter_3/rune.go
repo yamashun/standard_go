a2a04ae41f7f1d9490e639266a5a0eb6d83f19f1
山田 俊輔 <yshunske@gmail.com>
2019-03-04 11:11:35 +0900

    rune型 Unicodeコードポイントを表す特殊な整数型

---
+++ chapter_3/rune.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+	fmt.Println("a")
+	fmt.Printf("%v", 'a')
+	fmt.Println("")
+	fmt.Println("👪")
+	fmt.Printf("%v", '👪')
+	fmt.Println("")
+	fmt.Println("🐶")
+	fmt.Printf("%v", '🐶')
+	fmt.Println("")
+}
