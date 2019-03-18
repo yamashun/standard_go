be9322ed5318abb073b008dc3e51b7cf4eed9349
山田 俊輔 <yshunske@gmail.com>
2019-03-18 23:01:45 +0900

    色々なfor文 rubyでいうeachのような繰り返しもrangeを使うことで実現可能。使う場面が少ないかもしれないが文字列でrangeを使うときはrune型となるため注意が必要

--- chapter_3/for/main.go
+++ chapter_3/for/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
-	} 
+	}
+
+	i := 0
+	for {
+		fmt.Println(i)
+		i++
+		if i == 100 {
+			break
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 1 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	fruits := [3]string{"Apple", "Banana", "Cherry"}
+	for i, s := range fruits {
+		fmt.Printf("fruits[%d]=%s\n", i, s)
+	}
+
+	for i, r := range "ABC" {
+		fmt.Printf("[%d] -> %d\n", i, r)
+	}
 }
