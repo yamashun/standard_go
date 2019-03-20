dd1e6394d60e41503c11614f2d15cf9f1c5a2c71
山田 俊輔 <yshunske@gmail.com>
2019-03-20 23:43:50 +0900

    簡易文を使ったswitch

--- chapter_3/switch/main.go
+++ chapter_3/switch/main.go
@@ -20,4 +20,11 @@ func main() {
 		s += "E"
 	}
 	fmt.Println(s)
+
+	switch n :=2; n {
+	case 1, 3, 5, 7, 9:
+		fmt.Printf("%d is odd\n", n)
+	case 2, 4, 6, 8, 10:
+		fmt.Printf("%d is even\n", n)
+	}
 }
