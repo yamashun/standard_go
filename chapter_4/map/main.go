2b3bf21ab298fdd5e19da36c9a3644688980ff7d
山田 俊輔 <yshunske@gmail.com>
2019-04-08 22:26:04 +0900

    mapのリテラル

--- chapter_4/map/main.go
+++ chapter_4/map/main.go
@@ -5,19 +5,7 @@ import (
 )
 
 func main() {
-	m := make(map[int]string)
-
-	m[1] = "US"
-	m[81] = "Japan"
-	m[86] = "China"
+	m := map[int]string{1: "Taro", 2: "Hanako", 3:"Jiro"}
 
 	fmt.Println(m)
-
-	m2 := make(map[string]string)
-
-	m2["Yamada"] = "Taro"
-	m2["Sato"] = "Hanako"
-	m2["Yamada"] = "Jiro"
-	
-	fmt.Println(m2)
 }
