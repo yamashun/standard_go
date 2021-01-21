108ce19d8e1b901ab16aa4c493b562ddf9605002
yamashun <yshunske@gmail.com>
2021-01-21 22:37:26 +0900

    配列型へのポインタのデリファレンスの簡単な書き方

--- chapter_5/pointer/main.go
+++ chapter_5/pointer/main.go
@@ -6,10 +6,20 @@ func inc(p *int) {
 	*p++
 }
 
+func pow(p *[3]int) {
+	i := 0
+	for i < 3 {
+		(*p)[i] = (*p)[i] * (*p)[i]
+		i++
+	}
+}
+
 func main() {
-	i := 1
-	inc(&i)
-	inc(&i)
-	inc(&i)
-	fmt.Println(i)
+	a := [3]string{"Apple", "Banana", "Cherry"}
+	p := &a
+	fmt.Println(a[1])
+	fmt.Println(p[1])
+	p[2] = "Grape"
+	fmt.Println(a[2])
+	fmt.Println(p[2])
 }
