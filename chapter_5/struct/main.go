ee2982bd8dfc149afda3b09c14665a5d02217972
yamashun <yshunske@gmail.com>
2021-01-24 13:41:52 +0900

    エイリアスを使ってプログラムから複雑な型を取り除くことができる

--- chapter_5/struct/main.go
+++ chapter_5/struct/main.go
@@ -4,9 +4,24 @@ import "fmt"
 
 type MyInt int
 
+type (
+	IntPair     [2]int
+	Strings     []string
+	AreaMap     map[string][2]float64
+	IntsChannel chan []int
+)
+
 func main() {
 	var n1 MyInt = 5
 	n2 := MyInt(7)
 	fmt.Println(n1)
 	fmt.Println(n2)
+
+	pairt := IntPair{1, 2}
+	strs := Strings{"Apple", "Banana", "Cherry"}
+	amap := AreaMap{"Tokyo": {35.7, 129.7}}
+
+	fmt.Println(pairt)
+	fmt.Println(strs)
+	fmt.Println(amap)
 }
