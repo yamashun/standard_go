85d3cfb9ee7d2048ae092b63807479c2937ea350
yamashun <yshunske@gmail.com>
2021-01-29 22:20:57 +0900

    構造体を含む構造体

---
+++ chapter_5/struct2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Feed struct {
+	Name   string
+	Amount uint
+}
+
+type Animal struct {
+	Name string
+	Feed Feed
+}
+
+func main() {
+	a := Animal{
+		Name: "Monkey",
+		Feed: Feed{
+			Name:   "Banana",
+			Amount: 10,
+		},
+	}
+
+	fmt.Println(a.Name)
+	fmt.Println(a.Feed.Name)
+	fmt.Println(a.Feed.Amount)
+
+	a.Feed.Amount = 15
+	fmt.Println(a.Feed.Amount)
+}
