adb4e4e62348b30981ed553d2b19eaaa8d1777b1
yamashun <yshunske@gmail.com>
2021-01-31 17:58:04 +0900

    メソッドの戻り値

--- chapter_5/method/main.go
+++ chapter_5/method/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Point struct{ X, Y int }
 
@@ -8,7 +11,13 @@ func (p *Point) Render() {
 	fmt.Printf("<%d,%d>\n", p.X, p.Y)
 }
 
+func (p *Point) Distance(dp *Point) float64 {
+	x, y := p.X-dp.X, p.Y-dp.Y
+	return math.Sqrt(float64(x*x + y*y))
+}
+
 func main() {
-	p := &Point{X: 5, Y: 12}
-	p.Render()
+	p := &Point{X: 0, Y: 0}
+	result := p.Distance(&Point{X: 1, Y: 1})
+	fmt.Println(result)
 }
