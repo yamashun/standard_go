c56a606c73e136023ad78679f7398d928895bb0a
yamashun <yshunske@gmail.com>
2021-01-31 18:02:18 +0900

    レシーバーの型が異なれば同名のメソッドを定義できる

--- chapter_5/method/main.go
+++ chapter_5/method/main.go
@@ -5,19 +5,21 @@ import (
 	"math"
 )
 
-type Point struct{ X, Y int }
+type IntPoint struct{ X, Y int }
+type FloatPoint struct{ X, Y float64 }
 
-func (p *Point) Render() {
-	fmt.Printf("<%d,%d>\n", p.X, p.Y)
-}
-
-func (p *Point) Distance(dp *Point) float64 {
+func (p *IntPoint) Distance(dp *IntPoint) float64 {
 	x, y := p.X-dp.X, p.Y-dp.Y
 	return math.Sqrt(float64(x*x + y*y))
 }
 
+func (p *FloatPoint) Distance(dp *FloatPoint) float64 {
+	x, y := p.X-dp.X, p.Y-dp.Y
+	return math.Sqrt(x*x + y*y)
+}
+
 func main() {
-	p := &Point{X: 0, Y: 0}
-	result := p.Distance(&Point{X: 1, Y: 1})
+	p := &IntPoint{X: 0, Y: 0}
+	result := p.Distance(&IntPoint{X: 1, Y: 1})
 	fmt.Println(result)
 }
