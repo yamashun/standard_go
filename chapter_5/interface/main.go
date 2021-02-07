8f32c23316411e7739de437454b8c978dab57ad4
yamashun <yshunske@gmail.com>
2021-02-07 16:05:53 +0900

    errorインターフェースの例
    構造体の定義やメソッドの定義に、任意のインターフェースを実装しているなどの宣言は必要ない

---
+++ chapter_5/interface/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type MyError struct {
+	Message string
+	ErrCode int
+}
+
+func (e *MyError) Error() string {
+	return e.Message
+}
+
+func RaiseError() error {
+	return &MyError{Message: "エラーが発生しました", ErrCode: 1234}
+}
+
+func main() {
+	err := RaiseError()
+	fmt.Println(err.Error())
+}
